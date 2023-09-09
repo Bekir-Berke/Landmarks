04490c951cea0f43df8da8a4aae21fc0bce97bf5
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-09-10 00:20:55 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bekir Berke Yılmaz on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

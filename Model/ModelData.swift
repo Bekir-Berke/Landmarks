44fe520529322cc2f73920f534ec637b3f98968b
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-09-10 19:35:56 +0300

    Create LandmarkList, Navigation

---
+++ Model/ModelData.swift
@@ -0,0 +1,33 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Bekir Berke Yılmaz on 10.09.2023.
+//
+
+import Foundation
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: "json") else {
+        fatalError("Couldn't find (filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load (filename) from main bundle:\n(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        let decodedData = try decoder.decode(T.self, from: data)
+        return decodedData
+    } catch {
+        fatalError("Couldn't parse (filename) as (T.self):\n(error)")
+    }
+}
+
+var landmarks: [Landmark] = load("landmarkData")
+
---
+++ Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Bekir Berke Yılmaz on 10.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
